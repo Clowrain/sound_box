234d8bc2b199db99a70b0495a369c3716207365d
wanglei <wanglei@homolo.com>
2025-11-27 16:39:45 +0800

    fix: 修改发布脚本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.example.sound_box"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // Specify NDK version manually
+    ndkVersion = "27.0.12077973"  // Explicitly set the NDK version here
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +22,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sound_box"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +31,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
